ffef9f1a2c6ff6dc049660f674ff96e6f7d06e7f
Ricardo Miguel <ricardomigueldf@gmail.com>
2020-02-06 19:45:19 -0300

    v3 em 06-02-20

--- JS-CursoEmVideo/Aula12ex/ex015/script.js
+++ JS-CursoEmVideo/Aula12ex/ex015/script.js
@@ -1,1 +1,11 @@
-alert('Olá!')
+function verificar(){
+    //window.alert('Funcionou!') //teste aos poucos, usando o alert
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.legth == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO]: ')
+    }
+
+}
